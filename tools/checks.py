78be8cb800b94b4d8136aa701b4a99415485fcc0
ZuttoZutto <ZuttoSetto@gmail.com>
2025-04-23 11:17:37 +0300

    Delete tools directory

--- tools/checks.py
+++
@@ -1,8 +0,0 @@
-from flask_login import current_user, AnonymousUserMixin
-from flask import current_app
-
-def check_user_role():
-    with current_app.app_context():
-        if current_user.is_anonymous:
-            return 0
-        return current_user.RoleId
